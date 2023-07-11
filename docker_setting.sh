dc43c889ea065a8f30efe044cf343f10d8a0acbc
Juseong Park <76880288+jala1494@users.noreply.github.com>
2023-07-11 23:36:08 +0900

    Create docker_setting.sh

---
+++ docker_setting.sh
@@ -0,0 +1,18 @@
+sudo docker run --gpus all -it --net=host --privileged \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=unix$DISPLAY \
+    -v `pwd`/share:/root/share \
+    -w /root/share \
+    --name perma \
+    lhs223/cuda_18.04:base \
+    bash
+#some error occur
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+
+#when there's no key
+apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
+apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/7fa2af80.pub
